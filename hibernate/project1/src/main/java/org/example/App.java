b83874084e097ba0c8eacfaa8c82dfb88348ba69
rahul verma <rahulverma41328@gmail.com>
2024-07-06 12:57:02 +0530

    rahul verma

--- hibernate/project1/src/main/java/org/example/App.java
+++ hibernate/project1/src/main/java/org/example/App.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
@@ -10,11 +12,20 @@ import org.hibernate.cfg.Configuration;
 public class App 
 {
     public static void main( String[] args ) {
-        // System.out.println( "Hello World!" );
+         System.out.println( "Hello World!" );
 
-        Configuration cf = new Configuration();
-        cf.configure("hibernate.cfg.xml");
-        SessionFactory factory = cf.buildSessionFactory();
+         Configuration cfg = new Configuration();
+         cfg.configure("hibernate.cfg.xml");
+        SessionFactory factory = cfg.buildSessionFactory();
 
+        // creating student
+        Student student = new Student(23,"rahul","suratgarh");
+        Session session =  factory.openSession();
+        session.beginTransaction();
+        session.save(student);
+        session.getTransaction().commit();
+        session.close();
+
+        System.out.println(factory);
     }
 }
