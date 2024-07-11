8378647794398c8e3eea89ded8d110385e59048f
rahul verma <rahulverma41328@gmail.com>
2024-07-11 22:47:44 +0530

    rahul verma

--- hibernate/project1/src/main/java/org/example/App.java
+++ hibernate/project1/src/main/java/org/example/App.java
@@ -5,13 +5,17 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Date;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args ) {
+    public static void main( String[] args ) throws IOException {
          System.out.println( "Hello World!" );
 
          Configuration cfg = new Configuration();
@@ -20,9 +24,27 @@ public class App
 
         // creating student
         Student student = new Student(23,"rahul","suratgarh");
+
+        //creating address
+        Address ad = new Address();
+
+        ad.setStreet("street1");
+        ad.setCity("suratgarh");
+        ad.setOpen(true);
+        ad.setAddedDate(new Date());
+        ad.setX(1233);
+
+        // set image
+
+        FileInputStream fis = new FileInputStream("src/main/java/resources/image.png");
+
+        byte[] data = new byte[fis.available()];
+        fis.read(data);
+        ad.setImage(data);
         Session session =  factory.openSession();
         session.beginTransaction();
-        session.save(student);
+      //  session.save(student);
+        session.save(ad);
         session.getTransaction().commit();
         session.close();
 
